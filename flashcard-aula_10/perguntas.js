fc2428304e151f509de8be4ff32ee077249d08a2
Cawan16 <148549666+Cawan16@users.noreply.github.com>
2024-10-30 07:49:24 -0300

    Add files via upload

---
+++ flashcard-aula_10/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Futebol',
+    'Qual o nome do estadio do Barcelona?',
+    'Camp Nou'
+)
+
+criaCartao(
+    'Futebol',
+    'Qual o maior do sul?',
+    'O Grêmio'
+)
+
+criaCartao(
+    'Conhecimento geral',
+    'Qual a cor do cavalo branco de Napolean?',
+    'Branco'
+)
+
+criaCartao(
+    'Truco',
+    'Qual o nome da manilha de ouro?',
+    'Salmora'
+)
